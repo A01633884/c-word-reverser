3b0a7e23cdba323b553d10452016b152af93f7e5
A01633884 <a01633884@itesm.mx>
2022-03-09 03:23:57 +0000

    First Solution

--- word-reverser.c
+++ word-reverser.c
@@ -6,10 +6,30 @@ int main(){
     char c;
     char word[MAX];
     int i = 0;
+    int init = 0;
+    int wordSize = -1;
+    int temp;
 
-    while ( (c = getchar()) != '\n' ) {
+    while ( (c = getchar()) != '\n' && c != EOF ) {
 	word[i++] = c;
     }
 
+
+    for (i = 0; word[i] != '\0'; ++i){
+        wordSize = wordSize + 1;
+
+    }
+
+    while(wordSize > init){
+        temp = word[init];
+        word[init] = word[wordSize];
+        word[wordSize] = temp;
+
+        ++init;
+        --wordSize;
+    }
+
     printf("%s\n", word);
+    
+    return 0;
 }
